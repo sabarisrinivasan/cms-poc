71d89750333d6b98304f8e6a40a863f1e47aea96
sabarisrinivasan <sabarivasan522@gmail.com>
2025-10-16 12:08:47 +0530

    feat: :sparkles: created input component

--- my-strapi-api/config/plugins.ts
+++ my-strapi-api/config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_FROM_EMAIL'),
+        defaultReplyTo: env('SMTP_FROM_EMAIL'),
+      },
+    },
+  },
+});
---
+++ my-strapi-api/src/api/development/services/development.ts
@@ -0,0 +1,7 @@
+/**
+ * development service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::development.development');
